09eab4e57eaca170ed5df0cc42165376af576392
Luís Peixoto <lmpeixoto@gmail.com>
2020-02-25 14:24:31 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,21 @@ const  express = require('express')
 const app = express()
 const port = 3000
 
+const doseParacetamol = 20 // mg/Kg
+const concBenURon = 40 // mg/mL
+
+const calcDoseParacetamol = (peso) => {
+    return peso * doseParacetamol
+}
 
 
-app.listen(port, () => console.log('Node server initialized on port' + port))
+const calcQuantBenURon = (dose) => {
+    return dose/concBenURon
+}
+
+const dose1 = calcDoseParacetamol(10)
+console.log(dose1)
+console.log(calcQuantBenURon(dose1))
+
+
+app.listen(port, () => console.log('Node server initialized on port ' + port))
